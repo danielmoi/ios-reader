464a7e9827d598ea24dfc35b5c8b0bd0df4ec863
Daniel Moi <dmoixy@gmail.com>
2019-03-09 16:53:03 +1100

    Initial Commit

---
+++ Reader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reader
+//
+//  Created by DM on 9/3/19.
+//  Copyright © 2019 ampersandmoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
